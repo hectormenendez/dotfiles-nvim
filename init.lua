4acf702409430a2e137b76e2369483e8cbc4c43e
Hector Menendez <hector@gik.mx>
2023-08-02 06:57:07 -0600

    lua: refactored init.lua for improved readability.

--- init.lua
+++ init.lua
@@ -1,24 +1,8 @@
------------------------------------------------------------- PACKAGE MANAGER
-
-local path_lazy = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(path_lazy) then
-    vim.fn.system({
-        "git",
-	"clone",
-	"--filter=blob:none",
-	"https://github.com/folke/lazy.nvim.git",
-	"--branch=main",
-	path_lazy,
-    })
-end
-vim.opt.rtp:prepend(path_lazy)
-
-
 ------------------------------------------------------------ KEYBINDINGS
 ---------------------- These are the minimal keybindings needed for Lazy
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.mapleader = [[ ]]
+vim.g.maplocalleader = [[ ]]
 
 -- removes the original space keybinding
 vim.api.nvim_set_keymap(
@@ -37,8 +21,19 @@ vim.api.nvim_set_keymap(
 );
 
 
------------------------------------------------------------- INIT
---
-require("lazy").setup({
-    { import = "plugins" },
-});
+------------------------------------------------------------ PACKAGE MANAGER
+
+local path_lazy = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(path_lazy) then
+    vim.fn.system({
+        "git",
+	"clone",
+	"--filter=blob:none",
+	"--single-branch",
+	"https://github.com/folke/lazy.nvim.git",
+	path_lazy,
+    })
+end
+vim.opt.rtp:prepend(path_lazy)
+
+require("lazy").setup({{ import = "plugins" }});
