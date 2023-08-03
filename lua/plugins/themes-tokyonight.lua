38336a32aa4b820f8a03176ee2c74365b3d87844
Hector Menendez <hector@gik.mx>
2023-08-03 01:54:43 -0600

    lua: theme.tokyo: updated color of line light.

--- lua/plugins/themes-tokyonight.lua
+++ lua/plugins/themes-tokyonight.lua
@@ -11,15 +11,15 @@ return {
 	-- make telescope borderless
         on_highlights = function(hl, c)
             local prompt = "#2d3149"
-	    local indicator = "#262634"
-	    hl.CursorLine = { bg = indicator };
-	    hl.CursorColumn = { bg = indicator };
-	    hl.ColorColumn = { bg = indicator };
+            local indicator = "#191A26"
+            hl.CursorLine = { bg = indicator };
+            hl.CursorColumn = { bg = indicator };
+            hl.ColorColumn = { bg = indicator };
 
             -- hl.TelescopeNormal = { bg = c.bg_dark, fg = c.fg_dark }
             -- hl.TelescopeBorder = { bg = c.bg_dark, fg = c.bg_dark }
             -- hl.TelescopePromptNormal = { bg = prompt }
-	    -- hl.TelescopePromptBorder = { bg = prompt, fg = prompt }
+            -- hl.TelescopePromptBorder = { bg = prompt, fg = prompt }
             -- hl.TelescopePromptTitle = { bg = prompt, fg = prompt }
             -- hl.TelescopePreviewTitle = { bg = c.bg_dark, fg = c.bg_dark }
             -- hl.TelescopeResultsTitle = { bg = c.bg_dark, fg = c.bg_dark }
