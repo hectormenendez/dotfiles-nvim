d0ca1cd701b30901ce8f108646cc938e552f3cef
Hector Menendez <hector@gik.mx>
2023-08-03 00:24:16 -0600

    lua: Improved settings for treesitter.

---
+++ lua/plugins/code-treesitter.lua
@@ -0,0 +1,35 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+        ensure_installed = {
+	    "help",
+	    "lua",
+	    "javascript",
+	    "typescript",
+	    "rust",
+	    "python",
+	    "c",
+	    "vim",
+	    "vimdoc",
+	    "query",
+        },
+        
+        -- Install parsers synchronously (only applied to `ensure_installed`)
+        sync_install = false,
+        
+        -- Automatically install missing parsers when entering buffer
+        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+        auto_install = true,
+        
+        highlight = {
+            enable = true,
+            
+            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+            -- Using this option may slow down your editor, and you may see some duplicate highlights.
+            -- Instead of true it can also be a list of languages
+            additional_vim_regex_highlighting = false,
+        },
+    }
+}
