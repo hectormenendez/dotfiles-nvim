d2191ae56c1a3e3640d8aa221c2ee07ed9520b1e
Hector Menendez <hector@gik.mx>
2023-08-10 00:42:08 -0600

    lua: Added code-comments via Comment.nvim

---
+++ lua/plugins/code-comments.lua
@@ -0,0 +1,39 @@
+return {
+    "numToStr/Comment.nvim",
+    lazy = false,
+    opts = {
+        padding = true, -- Add a space b/w comment and the line
+        sticky = true, -- Whether the cursor should stay at its position
+        ignore = nil, -- Lines to be ignored while (un)comment
+        pre_hook = nil, -- Function to call before (un)comment
+        post_hook = nil, -- Function to call after (un)comment
+
+        -- LHS of toggle mappings in NORMAL mode
+        toggler = {
+            line = 'gcc', -- Line-comment toggle keymap
+            block = 'gbc', -- Block-comment toggle keymap
+        },
+
+        -- LHS of operator-pending mappings in NORMAL and VISUAL mode
+        opleader = {
+            line = 'gc', -- Line-comment keymap
+            block = 'gb', -- Block-comment keymap
+        },
+
+        -- LHS of extra mappings
+        extra = {
+            above = 'gcO', -- Add comment on the line above
+            below = 'gco', -- Add comment on the line below
+            eol = 'gcA', -- Add comment at the end of line
+        },
+
+        -- Enable keybindings. When `false`, plugin won't create any mappings
+        mappings = {
+            -- Operator-pending mapping;
+            -- `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+            basic = true,
+            -- Extra mapping; `gco`, `gcO`, `gcA`
+            extra = true,
+        },
+    },
+}
