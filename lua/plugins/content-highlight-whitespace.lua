db58b7a56070889eb825ff37cf61dffabdb0bea7
Hector Menendez <hector@gik.mx>
2023-08-10 17:56:14 -0600

    lua: added both whitespace and indent-line plugins.

---
+++ lua/plugins/content-highlight-whitespace.lua
@@ -0,0 +1,24 @@
+return {
+    "lukoshkin/highlight-whitespace",
+    config = true,
+    event = "VeryLazy",
+    opts = {
+        tws = '\\s\\+$',
+        clear_on_winleave = false,
+        user_palette = {
+            markdown = {
+                tws = 'RosyBrown',
+                ['\\(\\S\\)\\@<=\\s\\(\\.\\|,\\)\\@='] = 'CadetBlue3',
+                ['\\(\\S\\)\\@<= \\{2,\\}\\(\\S\\)\\@='] = 'SkyBlue1',
+                ['\\t\\+'] = 'plum4',
+            },
+            other = {
+                tws = 'PaleVioletRed',
+                ['\\(\\S\\)\\@<=\\s\\(,\\)\\@='] = 'coral1',
+                ['\\(\\S\\)\\@<= \\{2,\\}\\(\\S\\)\\@='] = 'LightGoldenrod3',
+                ['\\t\\+'] = 'plum4',
+            }
+        }
+    }
+}
+
