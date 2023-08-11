72c204fe734b2d149ce351f645a305e8b6e30cbe
Hector Menendez <hector@gik.mx>
2023-08-11 09:08:13 -0600

    lua: content-lsp-zero: fixed loading order.

--- lua/plugins/content-lsp-zero.lua
+++ lua/plugins/content-lsp-zero.lua
@@ -1,48 +1,62 @@
-local utils = require("etor.utils")
-
-utils.onload(function()
-    local lsp = require("lsp-zero").preset({})
-
-    lsp.on_attach(function(client, bufnr)
-        -- see :help lsp-zero-keybindings to learn the available actions
-        lsp.default_keymaps({ buffer = bufnr })
-    end)
-
-    lsp.extend_cmp()
-
-    require("mason").setup({})
-
-    require("mason-lspconfig").setup({
-        handlers = {lsp.default_setup},
-        ensure_installed = {
-            "tsserver",
-            "eslint",
-            "lua_ls",
-            "rust_analyzer"
-        },
-    })
-end)
-
 return {
+    "VonHeikemen/lsp-zero.nvim",
     -- TODO: on Sept 2023, this will be main, check that
-    { "VonHeikemen/lsp-zero.nvim", branch = "dev-v3" },
-
-    { "williamboman/mason.nvim" },
-    { "williamboman/mason-lspconfig.nvim" },
-
-    -- LSP Support
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            { "hrsh7th/cmp-nvim-lsp" },
+    branch = "dev-v3",
+    dependencies = {
+        -- LSP Support
+        {
+            "neovim/nvim-lspconfig",
+            dependencies = {
+                { "hrsh7th/cmp-nvim-lsp" },
+            },
         },
+
+        -- Autocompletion
+        {
+            "hrsh7th/nvim-cmp",
+            dependencies = {
+                { "L3MON4D3/LuaSnip" }
+            }
+        },
+
+        -- automatic servers handling
+        { "williamboman/mason.nvim" },
+        { "williamboman/mason-lspconfig.nvim" },
+
+        -- utilities for lua plugin development
+        { "folke/neodev.nvim", opts={} },
     },
 
-    -- Autocompletion
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            { "L3MON4D3/LuaSnip" },
-        }
-    }
+    config = function()
+        local lsp = require("lsp-zero").preset({})
+
+        lsp.on_attach(function(client, bufnr)
+            -- see :help lsp-zero-keybindings to learn the available actions
+            lsp.default_keymaps({ buffer = bufnr })
+        end)
+
+        lsp.extend_cmp()
+
+        local cmp = require("cmp")
+        cmp.setup({
+            mapping = {
+                ["<cr>"] = cmp.mapping.confirm({ select = false })
+            }
+        })
+
+        require("mason").setup({})
+
+        require("mason-lspconfig").setup({
+            handlers = { lsp.default_setup },
+            ensure_installed = {
+                "tsserver",
+                "eslint",
+                "lua_ls",
+                "rust_analyzer"
+            },
+        })
+
+        require("neodev").setup({})
+    end
 }
+
