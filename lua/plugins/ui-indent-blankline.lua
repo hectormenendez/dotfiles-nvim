ae19fb1a0fa1392987ceff7431534c56e98bb4fc
Hector Menendez <hector@gik.mx>
2023-08-13 18:43:18 -0600

    lua: ui-indent-blankline: line coloring finally works!

--- lua/plugins/ui-indent-blankline.lua
+++ lua/plugins/ui-indent-blankline.lua
@@ -10,11 +10,11 @@ utils.onload(function()
 
     utils.highlighter({
         IndentBlanklineChar = { guifg = theme_colors.lite },
+        IndentBlanklineContextChar = { guifg = theme_colors.liter, guibg = "None" },
+        -- IndentBlanklineContextStart = { underline = "red" },
         -- IndentBlanklineSpaceChar = { guifg = "cyan", guibg = "magenta" },
         -- IndentBlanklineSpaceCharBlankline = { guifg = "cyan", guibg = "magenta" },
-        IndentBlanklineContextChar = { guifg = "cyan", guibg = "magenta" },
         -- IndentBlanklineContextSpaceChar = { guifg = "cyan", guibg = "magenta" },
-        -- IndentBlanklineContextStart = { guifg = "cyan", guibg = "magenta" },
         -- IndentBlanklineIndent1 = { guifg = "cyan", guibg = "magenta" },
         -- IndentBlanklineIndent2 = { guifg = "cyan", guibg = "magenta" },
         -- IndentBlanklineIndent4 - { guifg = "cyan", guibg = "magenta" },
@@ -34,31 +34,27 @@ return {
         require("indent_blankline").setup {
             -- setting these to true seems attractive, but they cause the lines to
             -- break when spacing is found
-            use_treesitter = false,
+            use_treesitter = true,
             use_treesitter_scope = false,
             show_first_indent_level = false,
-            strict_tabs = true,
+            strict_tabs = false,
             buftype_exclude = {
                 "terminal",
                 "nofile",
                 "quickfix",
                 "prompt"
             },
-
-            -- as of Aug 11th 2023, this is not working for me
-            -- I'm leaving it anyways in case of a future update fixes it
             show_current_context = true,
             show_current_context_start = false,
-            show_current_context_start_on_current_line = true,
-            context_highlight_list = { 'Error', 'Warning' },
-            context_patterns = {
-                "class", "return", "function", "method",
-                "^if", "^while", "jsx_element", "^for",
-                "^object", "^table", "block", "arguments",
-                "if_statement", "else_clause", "jsx_element",
-                "jsx_self_closing_element", "try_statement",
-                "catch_clause", "import_statement", "operation_type"
-            },
+            show_current_context_start_on_current_line = false,
+            -- context_patterns = {
+            --     "class", "return", "function", "method",
+            --     "^if", "^while", "jsx_element", "^for",
+            --     "^object", "^table", "block", "arguments",
+            --     "if_statement", "else_clause", "jsx_element",
+            --     "jsx_self_closing_element", "try_statement",
+            --     "catch_clause", "import_statement", "operation_type"
+            -- },
         }
     end
 }
