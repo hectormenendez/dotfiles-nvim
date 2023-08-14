6c4189a47f07b3563481e500117164519d9e13a0
Hector Menendez <hector@gik.mx>
2023-08-13 18:20:55 -0600

    lua: content-flash: keymaps now come from etor.remaps.

--- lua/plugins/content-flash.lua
+++ lua/plugins/content-flash.lua
@@ -1,21 +1,7 @@
-local function flash_fwd()
-    require('flash').jump({ forward = true, wrap = false, multi_window = true })
-end
-
-local function flash_bak()
-    require("flash").jump({
-        search = { forward = false, wrap = false, multi_window = true }
-    })
-end
+local maps_go = require("etor.remaps.go");
 
 return {
     "folke/flash.nvim",
-    keys = {
-        { "gs", flash_fwd, desc="go: search: forwards" },
-        { "gS", flash_bak, desc="go: search: backwards" },
-        { "<C-s>", mode = { "i" }, flash_fwd, desc="go: search: forwards" },
-        -- for this to work you have to send the chord `\x1b[83;5u` from your terminal
-        { "<C-S-s>", mode = { "i" }, flash_bak, desc="go: search: backwards" }
-    },
+    keys = maps_go.flash,
     opts = {},
 }
