2222ab36e572b7f98d992abf269d149ac479c78d
Hector Menendez <hector@gik.mx>
2023-08-13 18:23:08 -0600

    lua: ui-bufferline: fixed erroneous color.

--- lua/plugins/ui-bufferline.lua
+++ lua/plugins/ui-bufferline.lua
@@ -23,7 +23,6 @@ return {
             color_icons = false,
             show_buffer_close_icons = false,
         },
-        -- TODO: This should be set in the theme, not here
         highlights = {
             fill = {
                 fg = "None",
@@ -47,7 +46,7 @@ return {
             },
             buffer_selected = {
                 fg = theme_colors.litest,
-                bg = 'None',
+                bg = "None",
                 bold = true,
                 italic = false,
             },
@@ -58,10 +57,253 @@ return {
             modified_visible = {
                 bg = theme_colors.darkst,
             },
-        };
-
+            duplicate = {
+                fg = theme_colors.lite,
+                bg = theme_colors.darkst,
+                italic = false,
+            },
+            -- tab = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- },
+            -- tab_selected = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- },
+            -- tab_separator = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- },
+            -- tab_separator_selected = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            --     sp = "yellow",
+            --     underline = "red",
+            -- },
+            -- tab_close = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- },
+            -- close_button = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- },
+            -- close_button_visible = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- },
+            -- close_button_selected = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- },
+            -- numbers = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- },
+            -- numbers_visible = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- },
+            -- numbers_selected = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            --     bold = true,
+            --     italic = true,
+            -- },
+            -- diagnostic = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- },
+            -- diagnostic_visible = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- },
+            -- diagnostic_selected = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            --     bold = true,
+            --     italic = true,
+            -- },
+            -- hint = {
+            --     fg = "cyan",
+            --     sp = "yellow",
+            --     bg = "magenta",
+            -- },
+            -- hint_visible = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- },
+            -- hint_selected = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            --     sp = "yellow",
+            --     bold = true,
+            --     italic = true,
+            -- },
+            -- hint_diagnostic = {
+            --     fg = "cyan",
+            --     sp = "yellow",
+            --     bg = "magenta",
+            -- },
+            -- hint_diagnostic_visible = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- },
+            -- hint_diagnostic_selected = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            --     sp = "yellow",
+            --     bold = true,
+            --     italic = true,
+            -- },
+            -- info = {
+            --     fg = "cyan",
+            --     sp = "yellow",
+            --     bg = "magenta",
+            -- },
+            -- info_visible = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- },
+            -- info_selected = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            --     sp = "yellow",
+            --     bold = true,
+            --     italic = true,
+            -- },
+            -- info_diagnostic = {
+            --     fg = "cyan",
+            --     sp = "yellow",
+            --     bg = "magenta",
+            -- },
+            -- info_diagnostic_visible = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- },
+            -- info_diagnostic_selected = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            --     sp = "yellow",
+            --     bold = true,
+            --     italic = true,
+            -- },
+            -- warning = {
+            --     fg = "cyan",
+            --     sp = "yellow",
+            --     bg = "magenta",
+            -- },
+            -- warning_visible = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- },
+            -- warning_selected = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            --     sp = "yellow",
+            --     bold = true,
+            --     italic = true,
+            -- },
+            -- warning_diagnostic = {
+            --     fg = "cyan",
+            --     sp = "yellow",
+            --     bg = "magenta",
+            -- },
+            -- warning_diagnostic_visible = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- },
+            -- warning_diagnostic_selected = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            --     sp = "yellow",
+            --     bold = true,
+            --     italic = true,
+            -- },
+            -- error = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            --     sp = "yellow",
+            -- },
+            -- error_visible = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- },
+            -- error_selected = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            --     sp = "yellow",
+            --     bold = true,
+            --     italic = true,
+            -- },
+            -- error_diagnostic = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            --     sp = "yellow",
+            -- },
+            -- error_diagnostic_visible = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- },
+            -- error_diagnostic_selected = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            --     sp = "yellow",
+            --     bold = true,
+            --     italic = true,
+            -- },
+            -- modified = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- },
+            -- modified_selected = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- },
+            -- duplicate_selected = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            --     italic = true,
+            -- },
+            -- duplicate_visible = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            --     italic = true,
+            -- },
+            -- indicator_visible = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- },
+            -- indicator_selected = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- },
+            -- pick_selected = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            --     bold = true,
+            --     italic = true,
+            -- },
+            -- pick_visible = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            --     bold = true,
+            --     italic = true,
+            -- },
+            -- pick = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            --     bold = true,
+            --     italic = true,
+            -- },
+            -- offset_separator = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- },
+            -- trunc_marker = {
+            --     fg = "cyan",
+            --     bg = "magenta",
+            -- }
+        },
     }
 }
-
-
-
