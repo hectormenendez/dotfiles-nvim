9faa820091748dc64148526d8894e77f79a67a20
Hector Menendez <hector@gik.mx>
2023-11-29 10:02:52 -0600

    lsp-zero: enabled denols for deno projects.

--- lua/plugins/content-lsp-zero.lua
+++ lua/plugins/content-lsp-zero.lua
@@ -89,6 +89,7 @@ return {
                 "zls", -- zig
                 "tsserver",
                 "eslint",
+                "denols",
             },
             handlers = {
                 lsp.default_setup,
@@ -133,5 +134,17 @@ return {
 
             -- NOTE: Insert keybindings here
         end)
+
+        local nvim_lsp = require('lspconfig')
+        nvim_lsp.denols.setup {
+            on_attach = on_attach,
+            root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
+        }
+
+        nvim_lsp.tsserver.setup {
+            on_attach = on_attach,
+            root_dir = nvim_lsp.util.root_pattern("package.json"),
+            single_file_support = false
+        }
     end
 }
