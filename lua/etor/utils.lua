00dae098b727faeb9755d1cadb60a3d8ba3b05f8
Hector Menendez <hector@gik.mx>
2025-01-19 18:57:08 -0600

    etor: utils

--- lua/etor/utils.lua
+++ lua/etor/utils.lua
@@ -7,6 +7,10 @@ function M.onload(callback)
     })
 end
 
+function M.string_trim(s)
+    return s:match '^()%s*$' and '' or s:match '^%s*(.*%S)'
+end
+
 -- Filters a table using given filter fn
 function M.table_filter(table, fn)
     for _, yanked in ipairs(table) do
@@ -56,13 +60,56 @@ function M.highlighter(highlights)
     end
 end
 
--- local registry for M.fn
+-- find a buffer by name
+function M.buffer_find(name)
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        buf_name = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(buf), ":t")
+        if (name == buf_name) then
+            return buf
+        end
+    end
+    return -1
+end
+
+function M.string_time()
+    return string.format("%s:%03d", os.date("%H:%M:%S"), vim.loop.now() % 1000)
+    -- return vim.loop.now()
+    -- return os.date("%H%M%S")
+end
+
+-- print to a buffer
+function M.buffer_print(...)
+    local args = { ... }
+    local buf_name = '|< !print'
+    local buf_id = M.buffer_find(buf_name)
+
+    if buf_id == -1 then
+        buf_id = vim.api.nvim_create_buf(true, true)
+        vim.api.nvim_buf_set_name(buf_id, buf_name)
+    end
+
+    for _, arg in ipairs(args) do
+        local time = M.string_time()
+        local lines = {}
+        for line in tostring(arg):gmatch("[^\r\n]+") do
+            table.insert(lines, time .. " | " .. line)
+        end
+        -- add each line to end of buffer
+        for _, line in ipairs(lines) do
+            vim.api.nvim_buf_set_lines(buf_id, -1, -1, false, { line })
+        end
+    end
+end
+
 local fn_cache = {}
 local function fn_register(fn)
     table.insert(fn_cache, fn)
-    return #fn_cache;
+    return #fn_cache
 end
-function M.fn_apply(id) fn_cache[id]() end
+
+function M.fn_apply(id)
+    fn_cache[id]()
+end
 
 function M.fn_cmd(fn)
     return string.format(
