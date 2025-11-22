2b71b53995d7f05a66111abc43964de08caea47b
Hector Menendez <hector@gik.mx>
2025-11-22 10:59:43 -0600

    content-highlight-whitespace: finally found a configuration that I like.

--- lua/plugins/content-highlight-whitespace.lua
+++ lua/plugins/content-highlight-whitespace.lua
@@ -5,18 +5,21 @@ return {
     opts = {
         tws = '\\s\\+$',
         clear_on_winleave = false,
-        user_palette = {
+        palette = {
             markdown = {
-                tws = 'RosyBrown',
-                ['\\(\\S\\)\\@<=\\s\\(\\.\\|,\\)\\@='] = 'CadetBlue3',
-                ['\\(\\S\\)\\@<= \\{2,\\}\\(\\S\\)\\@='] = 'SkyBlue1',
-                ['\\t\\+'] = 'plum4',
+                tws = "PaleVioletRed",
+                ["\\S\\@<=\\s\\(\\.\\|,\\)\\@="] = "CadetBlue3",
+                ["\\S\\@<= \\{2,\\}\\S\\@="] = "SkyBlue1",
+                ["\\t\\+"] = "plum4",
             },
             other = {
-                tws = 'PaleVioletRed',
-                ['\\(\\S\\)\\@<=\\s\\(,\\)\\@='] = 'coral1',
-                ['\\t\\+'] = 'plum4',
-            }
+                tws = "PaleVioletRed",
+                ["\\S\\@<=\\s,\\@="] = "coral1",
+                -- ["\\S\\@<=\\(#\\|--\\)\\@<! \\{2,3\\}\\S\\@=\\(#\\|--\\)\\@!"] = "LightGoldenrod3",
+                -- ["\\(#\\|--\\)\\@<= \\{2,\\}\\S\\@="] = "#3B3B3B",
+                -- ["\\S\\@<= \\{3,\\}\\(#\\|--\\)\\@="] = "#3B3B3B",
+                ["\\t"] = "plum4",
+            },
         }
     }
 }
